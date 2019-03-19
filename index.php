23a3af934751b6c52f70500213bef1b18bd2192e
Adi Nugroho <adinugroho696@gmail.com>
2019-03-19 13:16:19 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php 
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Process only when method is POST
+if($method == 'POST'){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+
+	$text = $json->result->parameters->text;
+
+	switch ($text) {
+		case 'Assalamualaikum':
+			$speech = "Waalaikumsalam";
+			break;
+
+		case 'Apa kabar':
+			$speech = "Kabar baik, bagaimana dengan anda ?";
+			break;
+
+		case 'Siapa kamu':
+			$speech = "Saya adalah virtual asisten dari restaoran ini";
+			break;
+		
+		default:
+			$speech = "Maaf saya kurang mengerti, silahkan bertanya yang lain.";
+			break;
+	}
+
+	$response = new \stdClass();
+	$response->speech = $speech;
+	$response->displayText = $speech;
+	$response->source = "webhook";
+	echo json_encode($response);
+}
+else
+{
+	echo "Method not allowed";
+}
+
+?>
